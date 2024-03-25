f2dad4a408362b5ad9f06203956715c138c4af78
ZManzoor25 <zainabmanzoor25@gmail.com>
2024-03-26 02:42:50 +0500

    cli number guessing game

---
+++ index.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+//1. computer will generate random number (done)
+
+//2. user input for guessing number (done)
+
+//3. compare user input with computer generated number and show result.
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("randomNumber");
+
+const answers = await inquirer.prompt([
+ {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "please guess a number between 1 - 6",
+  },
+]);
+
+if (answers.userGuessedNumber === randomNumber) {
+  console.log("Congratulations! you guessed right number");
+} else {
+    console.log("you guessed wrong number");
+};
